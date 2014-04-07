c34e0f7056fe9c29f9948a3d5b0dae72830d25e6
Quazi Nafiul Islam <gamesbrainiac@gmail.com>
2014-04-08 01:11:46 +0600

    Initial commit

---
+++ dirwatcher.py
@@ -0,0 +1,44 @@
+# encoding=utf-8
+
+import os
+import time
+import threading
+import sys
+
+from queue import Queue
+
+MAIN_TASK_QUEUE = Queue()
+
+
+def watch(directory):
+    print("Watching {}".format(directory))
+    old = set(os.listdir(directory))
+    while True:
+        time.sleep(0.5)
+        new = set(os.listdir(directory))
+        change = old ^ new
+        if change:
+            old = new
+            for c in change:
+                MAIN_TASK_QUEUE.put(c)
+
+
+def print_q():
+    while MAIN_TASK_QUEUE:
+        change = MAIN_TASK_QUEUE.get()
+        try:
+            assert isinstance(change, str)
+            print(change.split('__')[0])
+            sys.stdout.write('\a')
+            sys.stdout.flush()
+        except (AssertionError,):
+            print("{} is not a string".format(change))
+
+if __name__ == '__main__':
+    # dirs = sys.argv[1:]  # This is for command line arguments
+    dirs = [r'/Users/quazinafiulislam/Code/Python/DirWatcher', r'/Users/quazinafiulislam/Downloads']
+    for d in dirs:
+        threading.Thread(target=watch, args=(d,), daemon=True).start()
+    threading.Thread(target=print_q, args=(), daemon=True).start()
+    print("Press q to quick watching \n", file=sys.stdout)
+    if input() == 'q': pass
